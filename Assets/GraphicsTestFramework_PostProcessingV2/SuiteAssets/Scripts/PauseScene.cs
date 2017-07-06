94c73ba8ee7a2a71c06343cca6749cf3287883da
Matt Dean <matthewdean3d@gmail.com>
2017-07-06 17:43:59 +0100

    Added first pass tests for PostProcessing V2

---
+++ Assets/GraphicsTestFramework_PostProcessingV2/SuiteAssets/Scripts/PauseScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GraphicsTestFramework
+{
+    public enum EventType { Start, Call }
+
+    public class PauseScene : MonoBehaviour
+    {
+        public EventType eventType;
+
+        public Animator[] animators;
+        public ParticleSystem[] particleSystems;
+
+        private void Start()
+        {
+            if (eventType == EventType.Start)
+                Pause();
+        }
+
+        public void Pause()
+        {
+            foreach (Animator animator in animators)
+                animator.speed = 0;
+            foreach (ParticleSystem particleSystem in particleSystems)
+                particleSystem.Pause();
+        }
+    }
+}
+
